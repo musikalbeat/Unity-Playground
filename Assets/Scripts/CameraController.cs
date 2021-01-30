942e4823e0bcca3a2c1151b095a7a08575843852
Whitney Tran <wtran95@gmail.com>
2021-01-30 10:58:00 -0800

    Little Food Monsters Follow Player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 2f;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 20f;
+
+    private float zoom = 10f;
+
+    void Update()
+    {
+        zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * zoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
